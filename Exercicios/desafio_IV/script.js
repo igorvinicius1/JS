3a030785ec92d52b17e1b1eba4e1faff65ca1ea5
Igor Vinícius <102437841+igorvinicius1@users.noreply.github.com>
2022-05-14 22:18:18 -0300

    Add files via upload
    Tabuadinha

---
+++ Exercicios/desafio_IV/script.js
@@ -0,0 +1,24 @@
+function calcular() 
+{
+    var number = document.getElementById('n')
+    number = Number(number.value)
+    var tab = document.getElementById('tab')
+    var cont = 1
+
+    if(number == 0)
+    {
+        window.alert('Please enter with a number not equal zero')
+    }
+    else
+    {
+        tab.innerHTML = `` //limpa a tabuada após mudar o numero (PRECISA SER O ID DO SELECTION E NÃO O DA DIV)
+
+        for(cont = 1; cont <= 10; cont++)
+        {
+            var item = document.createElement('option')
+            item.text = `${number} x ${cont} = ${number*cont}`
+            tab.appendChild(item)
+        }
+    }
+    
+}
